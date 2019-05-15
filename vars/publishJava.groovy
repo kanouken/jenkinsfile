e539f967d322376164e08bf83b88b5aa051065d8
kanouken <1462927100@qq.com>
2019-05-15 11:27:34 +0800

    更新 publishJava.groovy

--- vars/publishJava.groovy
+++ vars/publishJava.groovy
@@ -3,23 +3,23 @@ def call(Map config){
     node {
  
     checkout scm
-   
+    def path  = config.path == null ? "" : "cd" + config.path + " &&"
     def gradle = tool 'gradle'
     def maven  = tool 'maven'
     stage('Build') {
        if(config.buildTool == 'gradle') {
-           sh "${gradle}/bin/gradle build"
-           sh "${gradle}/bin/gradle publishMavenJavaPublicationToMavenRepository"
+           sh "${path} ${gradle}/bin/gradle build"
+           sh "${path} ${gradle}/bin/gradle publishMavenJavaPublicationToMavenRepository"
        }else{
            //maven
-           sh "${maven}/bin/mvn clean deploy -Dmaven.test.skip=true"
+           sh "${path} ${maven}/bin/mvn clean deploy -Dmaven.test.skip=true"
        }
     }
    
     stage('SonarQube find bugs') {
         def sonarScanner = tool 'sonarScanner'
         withSonarQubeEnv() {
-            sh "${sonarScanner}/bin/sonar-scanner"
+            sh "${path} ${sonarScanner}/bin/sonar-scanner"
         }
     }
     build job: config.triggerJob, wait: false
