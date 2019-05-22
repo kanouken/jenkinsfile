d47e308716732727d62f998dc453b54beaa537b6
kanouken <1462927100@qq.com>
2019-05-22 14:18:02 +0800

    fix triggerjob null

--- vars/publishJava.groovy
+++ vars/publishJava.groovy
@@ -6,6 +6,7 @@ def call(Map config){
     def path  = config.path == null ? "" : "cd " + config.path + " &&"
     def gradle = tool 'gradle'
     def maven  = tool 'maven'
+    
     stage('Build') {
        if(config.buildTool == 'gradle') {
            sh "${path} ${gradle}/bin/gradle build"
@@ -22,6 +23,9 @@ def call(Map config){
             sh "${path} ${sonarScanner}/bin/sonar-scanner"
         }
     }
-    build job: config.triggerJob, wait: false
+    if(config.triggerJob && config.triggerJob != null){
+       build job: config.triggerJob + "/" + BRANCH_NAME, wait: false
+    }
+   
 }
 }
